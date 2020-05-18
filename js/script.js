d8687833be5884e06818c099c2cc215c3c0fc7b2
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-05-18 16:42:13 -0400

    Commiting Readme Update
    Updated readme for proper information and formatting.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JAVASCRIPT START
+
+console.log('JS File Is Linked!');
+
+let firstName = 'Jon'; // let is a newer version of var; const can also be used
+const lastName = 'Miller';
+
+console.log(lastName);
+
+console.log(`Hello ${firstName}`) //ES6 template literals
